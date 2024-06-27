d8995315f23980ed6be0c830775e9e591dff658f
Maxim Kornev <radioactiive@yandex.ru>
2024-06-27 11:24:43 +0300

    script is ready

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,27 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const container = document.querySelector('.content');
+const cardContainer = container.querySelector('.places__list');
+const cardTempate = document.querySelector('#card-template').content;
+
+function addCard(item) {
+  const cardElement = cardTempate.querySelector('.card').cloneNode(true);
+  const buttonDelete = cardElement.querySelector('.card__delete-button');
+
+  cardElement.querySelector('.card__title').textContent = item.name;
+  cardElement.querySelector('.card__image').src = item.link;
+  cardContainer.append(cardElement);
+
+  buttonDelete.addEventListener('click', deleteCard);
+}
+
+function deleteCard(evt) {
+  const eventTarget = evt.target;
+  eventTarget.closest('.card').remove();
+}
+
+initialCards.forEach(item => {
+  addCard(item);
+});
